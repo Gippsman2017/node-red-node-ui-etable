094fdf87a5b37adf20df6fe510be2e2dfb8eca0b
root <root@officepc.combatwombat.net>
2019-11-29 20:41:45 +1100

    added

---
+++ dev/new/node.js
@@ -0,0 +1,155 @@
+/**
+ * Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var path = require('path');
+
+module.exports = function (RED) {
+    function checkConfig(node, conf) {
+        if (!conf || !conf.hasOwnProperty('group')) {
+            node.error(RED._('table.error.no-group'));
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    function HTML(config,dark) {
+        console.log('PRE=',config);
+        var configAsJson = JSON.stringify(config);
+//        var configAsJson = config;
+        console.log('POST=',configAsJson);
+        var mid = (dark) ? "_midnight" : "";
+        var html = String.raw`
+                <link href='ui-table/css/tabulator`+mid+`.min.css' rel='stylesheet' type='text/css'>
+                <script type='text/javascript' src='ui-table/js/tabulator.js'></script>
+                <div id='ui_table-{{$id}}'></div>
+                <input type='hidden' ng-init='init(` + configAsJson + `)'>
+            `;
+        return html;
+    };
+
+    function TableNode(config) {
+        var done = null;
+        var node = this;
+        try {
+            RED.nodes.createNode(this, config);
+            if (checkConfig(node, config)) {
+                var ui = RED.require('node-red-dashboard')(RED);
+console.log(config);
+                var luma = 255;
+                if (ui.hasOwnProperty("getTheme") && (ui.getTheme() !== undefined)) {
+                    var rgb = parseInt(ui.getTheme()["page-sidebar-backgroundColor"].value.substring(1), 16);   // convert rrggbb to decimal
+                    luma = 0.2126 * ((rgb >> 16) & 0xff) + 0.7152 * ((rgb >>  8) & 0xff) + 0.0722 * ((rgb >>  0) & 0xff); // per ITU-R BT.709
+                }
+                if (config.height == 0) { config.height = 20; } // min height to 2 so auto will show something
+//                 config.columns=[{"title":"requestId", "field":"requestId", "editor":true}];
+config.columns = config.info;
+config.columns.replace(/'/g, "");
+console.log('\n\nconfig.info\n',config.info,'\n\n\n');
+var myColumns = config.info;
+delete config.info;
+
+                var html = HTML(config,(luma < 128));
+console.log('Columns after HTML=',config);
+console.log('HTML=',html);
+//config.columns = myColumns;
+console.log('Columns=',config);
+                done = ui.addWidget({
+                    node: node,
+                    width: config.width,
+                    height: config.height,
+                    format: html,
+                    templateScope: 'local',
+                    order: config.order,
+                    group: config.group,
+                    forwardInputMessages: false,
+                    beforeEmit: function (msg, value) {
+                        return { msg: { payload: value } };
+                    },
+                    beforeSend: function (msg, orig) {
+                        if (orig) { return orig.msg; }
+                    },
+                    initController: function ($scope, events) {
+                        $scope.inited = false;
+                        $scope.tabledata = [];
+                        var tablediv;
+                        var createTable = function(basediv, tabledata, columndata, outputs) {
+                            var y = (columndata.length === 0) ? 25 : 32;
+                            var opts = {
+                                data: tabledata,
+                                layout: 'fitColumns',
+                                columns: columndata,
+                                autoColumns: columndata.length == 0,
+                                movableColumns: true,
+pagination:"local",
+paginationSize:20,
+                                height: tabledata.length * y + 26
+                            }
+console.log(opts);
+                            if (outputs > 0) {
+                                opts.cellClick = function(e, cell) {
+                                    $scope.send({topic:cell.getField(),payload:cell.getData()});
+                                };
+                            }
+                            var table = new Tabulator(basediv, opts);
+                        };
+                        $scope.init = function (config) {
+                            $scope.config = config;
+                            tablediv = '#ui_table-' + $scope.$eval('$id')
+                            var stateCheck = setInterval(function() {
+                                if (document.querySelector(tablediv) && $scope.tabledata) {
+                                    clearInterval(stateCheck);
+                                    $scope.inited = true;
+                                    createTable(tablediv,$scope.tabledata,$scope.config.columns,$scope.config.outputs);
+                                    $scope.tabledata = [];
+                                }
+                            }, 40);
+                        };
+                        $scope.$watch('msg', function (msg) {
+                            if (msg && msg.hasOwnProperty("payload") && Array.isArray(msg.payload)) {
+                                if ($scope.inited == false) {
+                                    $scope.tabledata = msg.payload;
+                                    return;
+                                }
+console.log('MSG=',msg.payload,'\n-',$scope.config.columns);
+                                createTable(tablediv,msg.payload,$scope.config.columns,$scope.config.outputs);
+                            }
+                        });
+                    }
+                });
+            }
+        }
+        catch (e) { console.log(e); }
+
+        node.on('close', function () {
+            if (done) { done(); }
+        });
+    }
+
+    RED.nodes.registerType('ui_table', TableNode);
+
+    var uipath = 'ui';
+    if (RED.settings.ui) { uipath = RED.settings.ui.path; }
+    var fullPath = path.join(RED.settings.httpNodeRoot, uipath, '/ui-table/*').replace(/\\/g, '/');;
+    RED.httpNode.get(fullPath, function (req, res) {
+        var options = {
+            root: __dirname + '/lib/',
+            dotfiles: 'deny'
+        };
+        res.sendFile(req.params[0], options)
+    });
+};
---
+++ dev/original/README.md
@@ -0,0 +1,64 @@
+node-red-node-ui-table
+======================
+
+A Node-RED UI widget node which displays data as a table.
+
+## Install
+
+Either use the Editor - Menu - Manage Palette - Install option, or run the following command in your Node-RED user directory (typically `~/.node-red`) after installing Node-RED-dashboard.
+
+        npm i node-red-node-ui-table
+
+## Usage
+
+This table node expects `msg.payload` to contain an array of data, one object per row.
+Each data row object should have the same set of keys because the keys in the object are used as the column names.
+
+Both examples can be imported from the Node-RED Editor - Menu - Import - Examples
+
+### Simple Table
+
+With no configuration the node will try to create a table with equally spaced columns of simple text for each row provided, using the keys as column titles.
+
+![screenshot](https://raw.githubusercontent.com/node-red/node-red-ui-nodes/master/node-red-node-ui-table/screenshot.png)
+
+### Richer Table
+
+The columns can be configured manually. If so then only the `msg.payload` properties defined will be displayed. You can then also define the Title, Width, Alignment and Format of the column.
+
+![screenshot2](https://raw.githubusercontent.com/node-red/node-red-ui-nodes/master/node-red-node-ui-table/screenshot2.png)
+
+ - **Title**: Text for the column title (or blank).
+ - **Width**: Either a number of pixels or percentage of the overall table width. e.g. 150 or 20%. Leave blank for automatic, equally spaced to fill the available space.
+ - **Align**: Column alignment, left, centre or right.
+ - **Format**: Formatting of the input.
+   - **Plain Text** - Simple text values.
+   - **HTML** - Rich html to allow text Formatting - *NOTE*: this is raw un-sanitised HTML.
+   - **Link** - URL link to a web page.
+   - **Image** - Source (src) URL of an image to display.
+   - **Progress** - a progress bar from 0 to 100.
+   - **Traffic** - Red/Amber/Green indicator light set by numbers in the range 0-33-67-100.
+   - **Color** - HTML color name, or hex value (#rrggbb) to fill the cell.
+   - **Tick/Cross** - Tick or Cross symbol, boolean true/false, numeric 1/0 or text "1"/"0".
+   - **Stars** - Number of stars - numeric 0 to 5.
+   - **Row Number** - Current row number.
+
+
+### Example data
+
+```
+[
+    {
+        "Name": "Kazuhito Yokoi",
+        "Age": "35",
+        "Favourite Color": "red",
+        "Date Of Birth": "12/09/1983"
+    },
+    {
+        "Name": "Oli Bob",
+        "Age": "12",
+        "Favourite Color": "cyan",
+        "Date Of Birth": "12/08/2017"
+    }
+]
+```
